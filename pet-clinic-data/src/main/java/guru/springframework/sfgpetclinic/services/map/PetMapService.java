d9e2571d6a348cfc56c1f23c53a4ecf1da7dc646
Danijel Petanovic <danijel.petanovic@ericsson.com>
2019-07-18 19:08:26 +0200

    Added Speciality SD JPA Service. Small refactor on MapService implementation Closes #44

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -7,7 +7,7 @@ import guru.springframework.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
